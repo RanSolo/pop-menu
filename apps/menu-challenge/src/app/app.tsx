9ba5dc22f38a290d8e86b0d9f8ce1c6193e849cd
ransolo <ransolo@me.com>
2021-11-07 16:12:05 -0600

    Change headline to pass new test

--- apps/menu-challenge/src/app/app.tsx
+++ apps/menu-challenge/src/app/app.tsx
@@ -1,27 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { MenuItem } from '@pop-menu/api-interfaces';
+import React from 'react';
+import { Menu } from './menu/menu';
 
 export const App = () => {
-  const [item, setItem] = useState<MenuItem>({ title: '', imageUrl: '', price: 0, description: '' });
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then((r) =>setItem(r));
-  }, []);
-
   return (
-    <>
       <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to menu-challenge!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Extensible Build Framework"
-        />
+        <h1>Welcome to My Hip Restaraunt!</h1>
+        <Menu />
       </div>
-      <h2>{item.title}</h2>
-    </>
   );
 };
 
