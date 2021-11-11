a4447f21e3326340af10026afe0b9721cf7bb0c5
ransolo <ransolo@me.com>
2021-11-10 21:52:00 -0600

    add e2e tests and helpers

--- apps/menu-challenge-e2e/src/integration/app.spec.ts
+++ apps/menu-challenge-e2e/src/integration/app.spec.ts
@@ -1,19 +1,68 @@
-import { getGreeting, getTitle } from '../support/app.po';
+import {
+  getNewDescriptionInput,
+  getNewImageUrlInput,
+  getNewPriceInput,
+  getNewTitleInput
+} from '../support/app.po';
+
+import {
+  getRemoveBtn,
+  getGreeting,
+  getTitle,
+  getCancelBtn,
+  getDeleteBtn
+} from '../support/app.po';
 
 describe('menu-challenge', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see  `../support/app.po.ts` file
-    getGreeting().contains('Welcome to My Hip Restaraunt!');
+    getGreeting().contains("Welcome to Dylan's Diner!");
   });
+
   it('should display a menu item title', () => {
     // // Custom command example, see `../support/commands.ts` file
     // cy.login('my-email@something.com', 'myPassword');
 
     getTitle().contains('Shrimp and Chorizo Paella');
+  });
+
+  it('should confirm and delete', () => {
+    getTitle().contains('Shrimp and Chorizo Paella');
+    getRemoveBtn().first().click();
+    cy.contains('Confirm Delete').should('exist');
+
+    getCancelBtn().first().click();
+    getTitle().contains('Shrimp and Chorizo Paella');
+    //   cy.wait(3400);
+    cy.contains('Confirm Delete').should('not.exist');
+    getRemoveBtn().first().click();
+    getDeleteBtn().first().click();
+    getTitle().contains('Shrimp and Chorizo Paella').should('not.exist');
+  });
+
+  it('should add new Item', () => {
+    cy.contains('Add Item').click();
+    cy.contains('Add Menu Item').should('exist');
+    getNewTitleInput().type('Cheeseburger');
+    getNewPriceInput().type('12.99');
+    getNewDescriptionInput().type(
+      'A cheeseburger is a hamburger topped with cheese. Traditionally, the slice of cheese is placed on top of the meat patty. ... As with other hamburgers, a cheeseburger may include toppings such as lettuce, tomato, onion, pickles, bacon, mayonnaise, ketchup, and mustard.'
+    );
+
+    getNewImageUrlInput().type(
+      'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_21/2870431/190524-classic-american-cheeseburger-ew-207p.jpg'
+    );
+    cy.get(
+      '.MuiGrid-grid-xs-3 > .MuiButtonBase-root > .MuiButton-label'
+    ).click();
+    getTitle().contains('Cheeseburger');
+    // getCancelBtn().first().click();
+    // getTitle().contains('Shrimp and Chorizo Paella');
+    // //   cy.wait(3400);
+    // cy.contains('Confirm Delete').should('not.exist');
+    // getRemoveBtn().first().click();
+    // getDeleteBtn().first().click();
+    // getTitle().contains('Shrimp and Chorizo Paella').should('not.exist');
   });
 });
--- apps/menu-challenge-e2e/src/support/app.po.ts
+++ apps/menu-challenge-e2e/src/support/app.po.ts
@@ -1,2 +1,47 @@
 export const getGreeting = () => cy.get('h1');
-export const getTitle = () => cy.get('h2');
+
+export const getTitle = () =>
+  cy.get(
+    ':nth-child(1) > .MuiPaper-root > .MuiCardContent-root > .MuiGrid-container > :nth-child(1) > .MuiTypography-root'
+  );
+
+export const getAddBtn = () =>
+  cy.get('.MuiGrid-grid-xs-2 > .MuiButtonBase-root > .MuiButton-label');
+
+export const getNewTitleInput = () =>
+  cy.get(
+    ':nth-child(1) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input'
+  );
+
+export const getNewPriceInput = () =>
+  cy.get(
+    ':nth-child(2) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input'
+  );
+
+export const getNewDescriptionInput = () =>
+  cy.get(
+    ':nth-child(3) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input'
+  );
+
+export const getNewImageUrlInput = () =>
+  cy.get(
+    ':nth-child(4) > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input'
+  );
+
+export const getRemoveBtn = () =>
+  cy.get(
+    ':nth-child(1) > .MuiPaper-root > .MuiCardActions-root > .MuiGrid-root > div > .MuiButtonBase-root'
+  );
+
+export const getDeleteBtn = () =>
+  cy.get(
+    '.MuiDialogActions-root > .MuiButton-containedPrimary > .MuiButton-label'
+  );
+
+export const getCancelBtn = () =>
+  cy.get('.MuiDialogActions-root > :nth-child(1)');
+
+export const getDescription = () =>
+  cy.get(
+    ':nth-child(1) > .MuiPaper-root > .MuiCardContent-root > .MuiGrid-container > :nth-child(3) > .MuiTypography-root'
+  );
