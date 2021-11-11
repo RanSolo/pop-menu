ad0ddc285cb6c34ef972f731b061b0e55e6244c3
ransolo <ransolo@me.com>
2021-11-10 20:48:34 -0600

    Add multiline, adornment props to custom input component

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/button';
+export * from './lib/input';
 export * from './lib/features/delete-item-button';
 export * from './lib/features/new-item-button';
--- libs/ui/src/lib/input.tsx
+++ libs/ui/src/lib/input.tsx
@@ -1,31 +1,57 @@
 import { OnClick } from '@pop-menu/api-interfaces';
-import { Box, Button as MuiButton, Color, TextField } from '@material-ui/core';
+import {
+  Box,
+  Button as MuiButton,
+  Color,
+  makeStyles,
+  TextField
+} from '@material-ui/core';
 import { text } from 'express';
 import { ChangeEventHandler } from 'react';
 
+const useStyles = makeStyles({
+  input: {
+    marginTop: 0
+  }
+});
+
 /* eslint-disable-next-line */
 export interface UiInputProps {
-  label: string
-  name: string
-  value: string | number
-  onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> | undefined
+  label: string;
+  name: string;
+  value: string | number;
+  onChange:
+    | ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
+    | undefined;
+  adornment?: JSX.Element | undefined;
+  multiline?: boolean;
 }
 
 export function Input({
   label,
   name,
   value,
-  onChange, 
-  }: UiInputProps) {
+  onChange,
+  adornment,
+  multiline
+}: UiInputProps) {
+  const classes = useStyles();
   return (
-    <TextField 
+    <TextField
       fullWidth
-      variant="outlined"
+      variant="standard"
       placeholder={label}
       name={name}
       onChange={onChange}
       defaultValue={value === 0 ? '' : value}
       label={label}
+      margin="dense"
+      multiline={multiline}
+      InputProps={{
+        endAdornment: adornment,
+        className: classes.input,
+        'aria-label': 'input'
+      }}
     />
   );
 }
