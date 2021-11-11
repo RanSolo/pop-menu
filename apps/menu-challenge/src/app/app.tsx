dfefdc43ce69c448ab320d1fae900e53ab6a81b9
ransolo <ransolo@me.com>
2021-11-10 20:40:19 -0600

    Change name of restaurant

--- apps/menu-challenge/src/app/app.tsx
+++ apps/menu-challenge/src/app/app.tsx
@@ -3,10 +3,10 @@ import { Menu } from './menu/menu';
 
 export const App = () => {
   return (
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to My Hip Restaraunt!</h1>
-        <Menu />
-      </div>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Welcome to Dylan's Diner!</h1>
+      <Menu />
+    </div>
   );
 };
 
