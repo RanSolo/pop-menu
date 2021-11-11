a407f13f5a8f8f5c372035e1f7678407656d79f1
ransolo <ransolo@me.com>
2021-11-10 20:46:15 -0600

    FIx issue with item key  - Linter fixes

--- apps/menu-challenge/src/app/menu/menu.tsx
+++ apps/menu-challenge/src/app/menu/menu.tsx
@@ -1,8 +1,8 @@
-import { OnClick, MenuItem } from '@pop-menu/api-interfaces';
+import { OnClick, MenuItem, EditControls } from '@pop-menu/api-interfaces';
 import { NewItemButton } from '@pop-menu/ui';
 import { Item } from '../menu-item/menu-item';
-import Container from '@material-ui/core/Container'
-import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import { useEffect, useState } from 'react';
 
 export function Menu() {
@@ -17,30 +17,18 @@ export function Menu() {
   return (
     <div>
       <Container maxWidth="xl">
-        <Grid
-          container
-          direction='column'
-          justifyContent='flex-start'
-        >
-          <Grid item >
+        <Grid container direction="column" justifyContent="flex-start">
+          <Grid item>
             <h1>Menu</h1>
           </Grid>
           <Grid item>
-            <NewItemButton
-              items={items}
-              setItems={setItems}
-            />
+            <NewItemButton items={items} setItems={setItems} />
           </Grid>
         </Grid>
-        <br />
-        <Grid
-          container
-          direction='row'
-          spacing={2}
-        >
+        <Grid container direction="row" spacing={2}>
           {items.map((item) => (
-            <Item 
-              key={`${item?.title} ${item?.price}`}
+            <Item
+              key={`${item?.id}`}
               item={item}
               items={items}
               setItems={setItems}
